b434579d9be3aef5405593ce3c7bc5db30b97cd7
Craig Dunn <craig.dunn@microsoft.com>
2020-04-03 16:34:30 -0700

    initial screenhelper commit

---
+++ duo-screenhelper/Assets/SurfaceDuo/Plugins/Button.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Device.Display;
+using System;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        //Changes both color and font size
+        GUIStyle localStyle = new GUIStyle();
+        localStyle.normal.textColor = Color.blue;
+        localStyle.fontSize = 50;
+
+        var text = "Dual screen device? " + ScreenHelper.IsDualScreenDevice();
+        GUI.Label(new Rect(2.0f, 2.0f, 400, 20), text, localStyle);
+
+
+        var o = Screen.orientation;
+        Debug.Log("Screen.orientation: " + o);
+        
+        GUI.Label(new Rect(2.0f, 100.0f, 400, 20), "Screen orientation: "+o.ToString(), localStyle);
+
+        if (Screen.width == 2784)
+        {
+            GUI.backgroundColor = Color.gray;
+            var r = new Rect(x: 1350, y: 0, width: 84, height: 1800);
+            GUI.Box(r,"");
+        }
+        if (ScreenHelper.IsDualScreenDevice())
+        {
+            Debug.Log("Is DualScreen");
+
+            try
+            {
+                var displayMask = DisplayMask.FromResourcesRect();
+
+                Debug.Log("We have a DisplayMask");
+
+                foreach (var rect in displayMask.GetBoundingRects())
+                {
+                    Debug.Log("DisplayMask Rect: " + rect);
+                    Console.WriteLine("DisplayMask Rect: " + rect);
+                    GUI.Label(new Rect(2.0f, 50.0f, 400, 20), "DisplayMask Rect: " + rect, localStyle);
+
+                    GUI.backgroundColor = Color.green;
+
+                    if (o == ScreenOrientation.LandscapeLeft || o == ScreenOrientation.LandscapeRight)
+                    {   // wide
+                        var r = new Rect(x: rect.x - 25, y: rect.y, width: rect.width + 50, height: rect.height);
+                        GUI.Box(r, "Ah");
+                    }
+                    else
+                    {   // portrait - tall
+                        var r = new Rect(x: rect.y, y: rect.x - 25, width: rect.height, height: rect.width + 50);
+                        GUI.Box(r, "Ahhhhhhhhhhhhhhhhhhhh");
+                    }
+                }
+            }
+            catch (System.Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+    }
+}
---
+++ duo-screenhelper/Assets/SurfaceDuo/Plugins/ScreenHelper.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+
+namespace Microsoft.Device.Display
+{
+    internal class OnPlayer
+    {
+        public static T Run<T>(Func<AndroidJavaClass, T> runner)
+        {
+            if (runner == null)
+                throw new ArgumentNullException(nameof(runner));
+
+            using (var player = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            {
+                return runner(player);
+            }
+        }
+    }
+
+    public class DisplayMask : IDisposable
+    {
+        private readonly AndroidJavaObject _displayMask;
+
+        private DisplayMask(AndroidJavaObject displayMask)
+        {
+            _displayMask = displayMask;
+        }
+
+        public static DisplayMask FromResourcesRect()
+        {
+            Debug.Log("DisplayMask.FromResourcesRect: ");
+
+            var mask = OnPlayer.Run(p =>
+            {
+                var context = p.GetStatic<AndroidJavaObject>("currentActivity")
+                    .Call<AndroidJavaObject>("getApplicationContext");
+
+                using (var dm = new AndroidJavaClass("com.microsoft.device.display.DisplayMask"))
+                {
+                    return dm.CallStatic<AndroidJavaObject>("fromResourcesRect", context);
+                }
+            });
+
+            return new DisplayMask(mask);
+        }
+
+        public RectInt[] GetBoundingRects()
+        {
+            var jrects = _displayMask.Call<AndroidJavaObject>("getBoundingRects");
+            var size = jrects.Call<int>("size");
+
+            Debug.Log("BoundingRects size: " + size);
+
+            var rects = new RectInt[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                var jrect = jrects.Call<AndroidJavaObject>("get", i);
+
+                var left = jrect.Get<int>("left");
+                var top = jrect.Get<int>("top");
+                var width = jrect.Call<int>("width");
+                var height = jrect.Call<int>("height");
+
+                rects[i] = new RectInt(xMin: left, yMin: top, width: width, height: height);
+            }
+
+            return rects;
+        }
+
+        public void Dispose()
+        {
+            _displayMask?.Dispose();
+        }
+    }
+
+    public class ScreenHelper
+    {
+        public static bool IsDualScreenDevice()
+        {
+#if !UNITY_EDITOR && UNITY_ANDROID
+            try
+            {
+                return OnPlayer.Run(p => p
+                    .GetStatic<AndroidJavaObject>("currentActivity")
+                    .Call<AndroidJavaObject>("getApplicationContext")
+                    .Call<AndroidJavaObject>("getPackageManager")
+                    .Call<bool>("hasSystemFeature", "com.microsoft.device.display.displaymask"));
+            }
+            catch
+            {
+                return false;
+            }
+#else
+            return false;
+#endif
+        }
+    }
+}
