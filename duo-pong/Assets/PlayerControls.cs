d6fad2c8473149c222b52f4633940ec99f6fd583
Craig Dunn <craig.dunn@microsoft.com>
2020-04-03 20:13:10 -0700

    add multitouch

--- duo-pong/Assets/PlayerControls.cs
+++ duo-pong/Assets/PlayerControls.cs
@@ -6,120 +6,90 @@ public class PlayerControls : MonoBehaviour {
 
 	public KeyCode moveUp = KeyCode.W;
 	public KeyCode moveDown = KeyCode.S;
-	public float speed = 10.0f;
-	public float boundY = 2.5f;
+	public float speed = 10.0f; // HARDCODED
+
+	private float boundY = 2.5f; // HARDCODED
+	private float boundHeight = 5.0f; // HARDCODED
 	private Rigidbody2D rb2d;
-	
+	private Vector3 position;
+	private float height;
+	private float middleX;
 	private int lastScreenWidth = 0;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		rb2d = GetComponent<Rigidbody2D> ();
-		
-		height = (float)Screen.height;
-		middleX = (float)Screen.width / 2.0f;
-
-		bottomleft = Camera.main.ScreenToWorldPoint(new Vector3(Screen.height, Screen.width, 0), Camera.MonoOrStereoscopicEye.Mono);
-		Debug.LogError($"Start bottomleft: {bottomleft}");
 	}
-
-	private Vector3 position;
 	
-	private float height;
-	private float middleX;
-	private Vector3 bottomleft;
-
 	// Update is called once per frame
 	void Update ()
 	{
 		if (lastScreenWidth != Screen.width)
-		{
+		{	// detect rotation
 			lastScreenWidth = Screen.width;
 			height = (float)Screen.height;
 			middleX = (float)Screen.width / 2.0f;
-			bottomleft = Camera.main.ScreenToWorldPoint(new Vector3(Screen.height, Screen.width, 0), Camera.MonoOrStereoscopicEye.Mono);
-			Debug.LogError($"Update bottomleft: {bottomleft}");
 		}
-		
-		
-		var vel = rb2d.velocity;
-		if (Input.GetKey(moveUp))
-		{
-			vel.y = speed;
-		}
-		else if (Input.GetKey(moveDown))
-		{
-			vel.y = -speed;
-		}
-		else if (!Input.anyKey)
-		{
-			vel.y = 0;
-		}
-		rb2d.velocity = vel;
 
 		var pos = transform.position;
-		pos = RestrictBounds(pos);
-		transform.position = pos;
 
-		//Debug.Log($"AFTER KEY paddle-pos: {pos.x},{pos.y}");
-
+		// touch controls on mobile, at least two (one for each paddle)
 		if (Input.touchCount > 0)
 		{
-			Touch touch = Input.GetTouch(0);
-
-			// Move the paddle if the screen has the finger moving.
-			if (touch.phase == TouchPhase.Moved)
+			foreach (var touch in Input.touches)
 			{
-				Vector2 tpos = touch.position;
-				//Debug.Log($"TOUCH touch-pos: {tpos.x},{tpos.y}    paddle-pos: {pos.x},{pos.y}");
-				// 874/1800 = .48
-				// 5*.48 = 2.42
-				// 2.5 - 2.42 = 0.08
-				var tposYpercent = (1 - (tpos.y / height)); // % from bottom up, converted to top-down
-				var worldY = 5.0f * tposYpercent;
-				var worldYadjusted = 2.5f - worldY;
-
-				Debug.Log($"TOUCH touch-pos.y: {tpos.y}  tposYpercent:{tposYpercent}  worldY:{worldY}  worldYadjusted:{worldYadjusted}   bottomleft:{bottomleft.x},{bottomleft.y}  Screen.width:{Screen.width}");
-
-				if (tpos.x > middleX && pos.x > 0)
+				// Move the paddle if the screen has the finger moving.
+				if (touch.phase == TouchPhase.Moved)
 				{
-					//tpos.x = (tpos.x - width) / width;
-					//tpos.y = (tpos.y - height) / height;
-					position = new Vector3(pos.x, worldYadjusted, 0.0f);
-					//position = RestrictBounds(position);
-					transform.position = position;
-				}
-				else if (tpos.x < middleX && pos.x < 0)
-				{
-					//tpos.y = (tpos.y - height) / height;
-					//position = new Vector3(pos.x, tpos.y, 0.0f);
-					position = new Vector3(pos.x, worldYadjusted, 0.0f);
-					//position = RestrictBounds(position);
-					transform.position = position;
-				}
+					Vector2 tpos = touch.position;
 
-				//vel.y = speed;
+					var tposYpercent = (1 - (tpos.y / height)); // % from bottom up, converted to top-down
+					var worldY = boundHeight * tposYpercent;
+					var worldYadjusted = boundY - worldY;
+
+					Debug.Log($"TOUCH touch-pos.y: {tpos.y}  tposYpercent:{tposYpercent}  worldY:{worldY}  worldYadjusted:{worldYadjusted}");
+
+					if (tpos.x > middleX && pos.x > 0)
+					{
+						position = new Vector3(pos.x, worldYadjusted, 0.0f);
+						transform.position = position;
+					}
+					else if (tpos.x < middleX && pos.x < 0)
+					{
+						position = new Vector3(pos.x, worldYadjusted, 0.0f);
+						transform.position = position;
+					}
+				}
 			}
 		}
-		//else
-		//{
-		//	// no touch
-		//	vel.y = 0;
-		//}
-		//rb2d.velocity = vel;
-
-		Vector3 RestrictBounds(Vector3 proposedPos)
-		{
-			if (proposedPos.y > boundY)
+		else 
+		{	// no touches, might as well check the keyboard
+			// legacy, for testing in Unity
+			var vel = rb2d.velocity;
+			if (Input.GetKey(moveUp))
 			{
-				proposedPos.y = boundY;
+				vel.y = speed;
 			}
-			else if (proposedPos.y < -boundY)
+			else if (Input.GetKey(moveDown))
 			{
-				proposedPos.y = -boundY;
+				vel.y = -speed;
 			}
+			else if (!Input.anyKey)
+			{
+				vel.y = 0;
+			}
+			rb2d.velocity = vel;
 
-			return proposedPos;
+			if (pos.y > boundY)
+			{
+				pos.y = boundY;
+			}
+			else if (pos.y < -boundY)
+			{
+				pos.y = -boundY;
+			}
+			transform.position = pos;
 		}
 	}
 }
